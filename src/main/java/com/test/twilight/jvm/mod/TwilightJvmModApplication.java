a1dc2a11ddfb11f0755d6fd9d9e2b9073f272b87
郭俊 <guojun@douyu.tv>
2020-08-05 09:35:49 +0800

    Initial commit

---
+++ src/main/java/com/test/twilight/jvm/mod/TwilightJvmModApplication.java
@@ -0,0 +1,13 @@
+package com.test.twilight.jvm.mod;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwilightJvmModApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TwilightJvmModApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/test/twilight/jvm/mod/TwilightJvmModApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.twilight.jvm.mod;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TwilightJvmModApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
